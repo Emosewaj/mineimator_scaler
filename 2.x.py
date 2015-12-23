7ae26710a12e08f748388e43b7fa411c0a1139b2
Jawesome99 <Jan.Dassow@gmx.de>
2015-12-23 04:35:54 +0100

    Create 2.x.py

---
+++ 2.x.py
@@ -0,0 +1,7 @@
+#Use this with http://www.skulpt.org or an installed python version 2.x
+
+size = input('Enter the wanted size in centimeters: ')
+size = float(size)
+scale = size / 180
+print'Your scale is',scale,''
+print'You can use this value as is in Mine-imator.'
