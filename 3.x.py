a2a2d22f98adef93339daf53512d4bc47cf969eb
Jawesome99 <Jan.Dassow@gmx.de>
2015-12-23 04:37:09 +0100

    Use this with Python 3.x

---
+++ 3.x.py
@@ -0,0 +1,7 @@
+#Use this with python version 3.x if installed
+
+size = input('Enter the wanted size in centimeters: ')
+size = float(size)
+scale = size / 180
+print('Your scale is',scale,)
+print('You can use this value as is in Mine-imator.')
